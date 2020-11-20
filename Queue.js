047c86a8eac4c55b2b4028eb7aa3ed4647f06f82
Raj <rajkamal11126@gmail.com>
2020-11-21 04:15:15 +0530

    Added : Functionality to dequeue the element

--- Queue.js
+++ Queue.js
@@ -2,7 +2,9 @@ const Utility = require('./Utility');
 let readlineSync = require('readline-sync')
 /** enter the element in the queue */
 let element = readlineSync.question("Enter the element:");
-let result = Utility.enqueue(element);
-console.log(result);               
-
+let enqueueElement = Utility.enqueue(element);
+console.log(enqueueElement); 
+/** deleted element in the queue */              
+let dequeueElement = Utility.dequeue();
+console.log(dequeueElement);
  
--- Utility.js
+++ Utility.js
@@ -5,12 +5,25 @@ class Queue {
         this.rear = 0;
         this.size = 10;
     }
-/** method for push the element */
+    /** method for push the element */
     enqueue = (element) => {
         if (this.rear < this.size) {
             this.data[this.rear] = element;
             this.rear = this.rear + 1;
             return this.rear;   // return the element
+        }
+    }
+    isEmpty() {
+
+        return this.rear === 0;
+    }
+    /** method for delete the element */
+    dequeue() {
+
+        if (this.isEmpty() === false) {
+
+            this.rear = this.rear - 1;
+            return this.data.shift(); //return the deleted element
         }
     }
 }
